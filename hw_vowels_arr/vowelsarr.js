f57a4c3d7be8a0461de5c188406d863d5e833fbf
Dmitry Dziraev <dziraevdmitry@gmail.com>
2022-08-19 09:41:36 +0300

    add four function for counting vowels(reduce, filter, reg, foreach)

---
+++ hw_vowels_arr/vowelsarr.js
@@ -0,0 +1,26 @@
+function vowelsFilter(str) {
+    const vowels = ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я'];
+    return str.toLowerCase().split('').filter(i => vowels.includes(i)).length;
+}
+
+function vowelsFor(str) {
+    const vowels = ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я'];
+    let count = 0;
+    str.toLowerCase().split('').forEach(i => ~vowels.indexOf(i) ? count+=1 : null);
+    return count;
+}
+
+function vowelsReduce(str) {
+    const vowels = ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я'];
+    return str.toLowerCase().split('').reduce((acc, curr) => acc + vowels.includes(curr), 0);
+}
+
+const vowelsReg = (str) => str.match(/[аеёиоуыэюя]/gi).length;
+
+
+const stroka = 'В этой строке семь гласных';
+
+console.log(vowelsFilter(stroka));
+console.log(vowelsReduce(stroka));
+console.log(vowelsFor(stroka));
+console.log(vowelsReg(stroka));
